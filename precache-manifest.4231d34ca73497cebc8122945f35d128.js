fb0238eabab4668b682d6a98250fffed19753109
Daniil Molodkov <dmolodkov@brainstation.io>
2020-05-29 13:22:44 -0400

    Updates

---
+++ precache-manifest.4231d34ca73497cebc8122945f35d128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28d1c1548a372dded6284b96081b0c8",
+    "url": "/pentry/index.html"
+  },
+  {
+    "revision": "06fc6d6bdc0d5947fef3",
+    "url": "/pentry/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "50e660f4effd98683e28",
+    "url": "/pentry/static/js/2.5e9cb0e7.chunk.js"
+  },
+  {
+    "revision": "567ff855e89811c94bb38895efc27afb",
+    "url": "/pentry/static/js/2.5e9cb0e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06fc6d6bdc0d5947fef3",
+    "url": "/pentry/static/js/main.a87123dd.chunk.js"
+  },
+  {
+    "revision": "a8a08b0b90061c43425c",
+    "url": "/pentry/static/js/runtime-main.f5ba1f6c.js"
+  }
+]);
